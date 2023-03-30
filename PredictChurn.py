25b07a437d8f0b6a80adcf35b9c6cac71e7795a3
Swasthik Bangera <swasthik.bangera@gmail.com>
2023-03-30 10:19:58 -0700

    Uploading files

---
+++ PredictChurn.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 28 09:28:26 2023
+
+@author: SwasthikBangera
+"""
+#importing dependencies
+
+import numpy as np
+import pandas as pd
+
+#importing dataset
+
+churn_data = pd.read_csv('/Users/yennamac/Downloads/train.csv')
+
+
+# Exploring dataset
+
+#print(churn_data.head())
